0907fb2b312ea574231e5bb60c78ec4d3ebf907f
James Benedict <bloop@jamess-mbp.home>
2017-10-28 13:56:50 -0400

    stage 3 complete of tutorial

---
+++ admin/project/academy/management/commands/loadacademycsv.py
@@ -0,0 +1,17 @@
+import csv
+from academy.models import Invite
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print "Loading CSV"
+        csv_path = "./academy_invites_2014.csv"
+        csv_file = open(csv_path, 'rb')
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+        	obj = Invite.objects.create(
+        		name=row['Name'],
+        		branch = row['Branch']
+        	)
+        	print obj
